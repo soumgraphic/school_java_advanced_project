202a5b02c051e1a77b2dc6bac17b33cc5bd72a9d
Soumaila <diarra176@gmail.com>
2022-03-16 17:51:42 +0100

    Java doc added in student business.

--- src/main/java/business/StudentBusiness.java
+++ src/main/java/business/StudentBusiness.java
@@ -9,7 +9,19 @@ import java.util.List;
 import java.util.Optional;
 
 public interface StudentBusiness {
+    /**
+     * Get student by id
+     *
+     * @param id student identifiant
+     * @return Student find by id
+     */
     Optional<Student> get(String id);
+
+    /**
+     * Get student list
+     *
+     * @return list of students
+     */
     List<Student> getList();
     Optional<Classroom> getClassroom(String id);
     Optional<School> getSchool(String id);
